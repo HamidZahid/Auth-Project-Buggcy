d24deecf12727e81bbcf10d013db6a14470edd67
HamidZahid <hamidchaudhary3867@gmail.com>
2024-08-22 14:31:15 +0500

    Server.js file Added only For deployment

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "cKCAGIKVGip/YzPhjEsU+6DcMPO767y0/rSnjh0Rc5k=",
+    EMAIL: "juston.lehner@ethereal.email", // testing email & password
+    PASSWORD : "Fpszad3A3ArWfdFHt3",
+    ATLAS_URI: "mongodb+srv://hamidchaudhary3867:CxAnYPocnLk802sP@auth.yo0dj.mongodb.net/"
+}
