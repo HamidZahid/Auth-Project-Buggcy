98cb8830e85020334b72b5d7c76b0c70b997922f
HamidZahid <hamidchaudhary3867@gmail.com>
2024-08-23 12:26:52 +0500

    Updated

--- server/src/App.js
+++ server/src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 
-/** import all components */
-
-/** auth middleware */
-import { AuthorizeUser, ProtectRoute } from "./middleware/auth";
-
-/** root routes */
 
 export default function App() {
   return (
     <main>
       <>
         <h1>Hello World</h1>
+        
       </>
     </main>
   );
